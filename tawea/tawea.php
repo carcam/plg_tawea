1a917f519d945b5bb5711ad043d6ccbd0dd9a2b7
Carlos Cámara <cmcamara@gmail.com>
2013-10-09 20:25:21 +0200

    initial development

---
+++ tawea/tawea.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author       Carlos M. Cámara
+ * @copyright    Copyright (C) 2013 Carlos M. Cámara Mora. All rights reserved.
+ * @license      GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+/**
+ * Tawea plugin.
+ *
+ * @package	Joomla.Plugin
+ * @subpackage	System.Tawea
+ */
+class plgSystemTawea extends JPlugin
+{
+	/**
+	 * Constructor
+	 *
+	 * @access      protected
+	 * @param       object  $subject The object to observe
+	 * @param       array   $config  An array that holds the plugin configuration
+	 * @since       1.5
+	 */
+	public function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
+
+	/**
+	* @since	1.6
+	*/
+	public function onAfterRender($context, &$row, &$params, $page=0)
+	{
+		$html = '';
+
+		//Get Params
+		$whenToShow = $this->params->def('whentoshow');
+
+		
+
+                                    
+		return $html;
+	}
+}
